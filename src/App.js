79dac462bf1c0c092055177ae4e2321b5cfc7836
Dagim <dagimgetaW27@gmail.com>
2024-04-25 18:27:17 +0300

    update the page layout

--- src/App.js
+++ src/App.js
@@ -74,10 +74,23 @@ function Form({ onAddItems }) {
   );
 }
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+
+  let sortedItems;
+
+  if (sortBy === "input") sortedItems = items;
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -86,6 +99,14 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sorted by input order</option>
+          <option value="description">Sorted by description</option>
+          <option value="packed">Sorted by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
